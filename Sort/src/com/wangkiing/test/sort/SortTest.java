665f2118eb8c9fb1fa916cbd3e4c9eeef134e6be
wangkiing <997328106@qq.com>
2017-05-03 21:51:32 +0800

    创建工程，实现直接插入排序，希尔排序，归并排序以及堆的建立

---
+++ Sort/src/com/wangkiing/test/sort/SortTest.java
@@ -0,0 +1,90 @@
+package com.wangkiing.test.sort;
+
+import org.junit.Test;
+
+import com.wangkiing.util.sort.SortUtil;
+
+
+public class SortTest {
+	
+	/**
+	 * 直接插入排序
+	 */
+	@Test
+	public void testInsertSort() {
+		int[] args = new int[10];
+		for(int i =0;i<10;i++){
+			args[i] = (int) (Math.random()*100);
+		}
+		
+		System.out.println("插入排序前:"+SortUtil.argsToString(args));
+		long before = System.currentTimeMillis();
+		SortUtil.insertSort(args);
+		long after = System.currentTimeMillis();
+		System.out.println("插入排序后:"+SortUtil.argsToString(args));
+		System.out.println("插入排序时间:"+(after-before));
+	}
+	
+	/**
+	 * 希尔排序
+	 */
+	@Test
+	public void testShellSort() {
+		int[] args = new int[10];
+		for(int i =0;i<10;i++){
+			args[i] = (int) (Math.random()*100);
+		}
+		
+		System.out.println("希尔排序前:"+SortUtil.argsToString(args));
+		long before = System.currentTimeMillis();
+		SortUtil.shellSort(args);
+		long after = System.currentTimeMillis();
+		System.out.println("希尔排序后:"+SortUtil.argsToString(args));
+		System.out.println("希尔排序时间:"+(after-before));
+	}
+
+	/**
+	 * 归并排序
+	 */
+	@Test
+	public void testMergeSort() {
+		int[] args1 = new int[11];
+		int[] args2 = new int[11];
+		for(int i =0;i<11;i++){
+			args1[i] = (int) (Math.random()*100);
+			args2[i] = (int) (Math.random()*100);
+		}
+		
+		System.out.println("递归归并排序前:"+SortUtil.argsToString(args1));
+		long before1 = System.currentTimeMillis();
+		SortUtil.mergeSortRecursion(args1);
+		long after1 = System.currentTimeMillis();
+		System.out.println("递归归并排序后:"+SortUtil.argsToString(args1));
+		System.out.println("递归归并排序时间:"+(after1-before1));
+		
+		System.out.println("非递归归并排序前:"+SortUtil.argsToString(args2));
+		long before2 = System.currentTimeMillis();
+		SortUtil.mergeSortNotRecursion(args2);
+		long after2 = System.currentTimeMillis();
+		System.out.println("非递归归并排序后:"+SortUtil.argsToString(args2));
+		System.out.println("非递归归并排序时间:"+(after2-before2));
+	}
+	
+	/**
+	 * 堆排序
+	 */
+	@Test
+	public void testHeapSort() {
+		int[] args = new int[10];
+		for(int i =0;i<10;i++){
+			args[i] = (int) (Math.random()*100);
+		}
+		
+		System.out.println("堆排序前:"+SortUtil.argsToString(args));
+		long before = System.currentTimeMillis();
+		SortUtil.heapSort(args);
+		long after = System.currentTimeMillis();
+		System.out.println("堆排序后:"+SortUtil.argsToString(args));
+		System.out.println("堆排序时间:"+(after-before));
+	}
+}
